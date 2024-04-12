d96d846fd138cdb2c7389437389de7fb5491403d
서혜리 <hye_dam_2@naver.com>
2024-04-12 13:04:19 +0900

    p.237까지 생성자 주입 방식

--- springex/src/main/java/org/zerock/springex/sample/SampleService.java
+++ springex/src/main/java/org/zerock/springex/sample/SampleService.java
@@ -1,14 +1,21 @@
 package org.zerock.springex.sample;
 
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service    // 도메인 기능에 부여
 @ToString
+@RequiredArgsConstructor
 public class SampleService {
 
-    // 필드 주입 방식
-    @Autowired
+    // lombok의 @RequiredArgsConstructor을 사용해서 '생성자 주입 방식'으로 객체를 주입함
+    // 반드시 final로 선언해야 함
+    private final SampleDAO sampleDAO;
+
+    // 필드 주입 방식 - 예전에 쓰던 방식
+    /* @Autowired
     private SampleDAO sampleDAO;
+    */
 }
