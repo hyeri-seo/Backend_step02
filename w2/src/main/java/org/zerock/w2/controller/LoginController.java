8d0ae7d7b5d0656c89badd7ba0056d29c449ae5f
서혜리 <hye_dam_2@naver.com>
2024-04-12 11:50:31 +0900

    p. 220까지 리스너

--- w2/src/main/java/org/zerock/w2/controller/LoginController.java
+++ w2/src/main/java/org/zerock/w2/controller/LoginController.java
@@ -28,27 +28,38 @@ public class LoginController extends HttpServlet {
         String mid = req.getParameter("mid");
         String mpw = req.getParameter("mpw");
 
+        // UUID로 자동로그인 할 것인지 여부
         String auto = req.getParameter("auto");
 
+        // login.jsp에 name=auto가 check되어 있다면, rememberMe는 true
         boolean rememberMe = auto != null && auto.equals("on");
 
 
         try {
+            // 기존 로그인
+            // id와 pw가 일치할 경우 memberDTO를 session 공간에 저장함
             MemberDTO memberDTO = MemberService.INSTANCE.login(mid, mpw);
 
+            // 자동로그인 설정이 되어 있는 경우
             if(rememberMe) {
+                // 유니크한 문자열을 생성할 때 사용함
                 String uuid = UUID.randomUUID().toString();
 
+                // MariaDB에 uuid를 저장
                 MemberService.INSTANCE.updateUuid(mid, uuid);
+                // loginInfo로 사용할 memberDTO에 uuid 저장
                 memberDTO.setUuid(uuid);
 
+                // remember-me라는 이름의 쿠키를 새로 생성
                 Cookie rememberCookie = new Cookie("remember-me", uuid);
                 rememberCookie.setMaxAge(60*60*24*7);   // 쿠키의 유효기간은 1주일
                 rememberCookie.setPath("/");
 
+                // 브라우저에 응답을 보낼 때 remember-me도 같이 전송해라
                 resp.addCookie(rememberCookie);
             }
 
+            // 세션 공간에 memberDTO를 loginInfo라는 key값으로 저장
             HttpSession session = req.getSession();
             session.setAttribute("loginInfo", memberDTO);
             resp.sendRedirect("/todo/list");
--- w2/src/main/java/org/zerock/w2/filter/LoginCheckFilter.java
+++ w2/src/main/java/org/zerock/w2/filter/LoginCheckFilter.java
@@ -29,16 +29,17 @@ public class LoginCheckFilter implements Filter {
 
         HttpSession session = req.getSession();
 
+        // 로그인 인포가 없는 경우 요청을 보냄
         if(session.getAttribute("loginInfo") == null) {
             filterChain.doFilter(req, resp);
             return;
         }
 
-        // session에 loginInfo 값이 없다면
+        // session에 loginInfo 값이 없지만, remember-me를 보냈는지 찾음
         // 쿠키를 체크
         Cookie cookie = findCookie(req.getCookies(), "remember-me");
 
-        // 세션에도 없고 쿠키도 없다면 그냥 로그인으로
+        // remember-me가 세션에도 없고 쿠키도 없다면 그냥 로그인하라고 /login으로 보냄
         if(cookie == null) {
             resp.sendRedirect("/login");
             return;
@@ -46,18 +47,22 @@ public class LoginCheckFilter implements Filter {
 
         // 쿠키가 존재하는 상황이라면
         log.info("cookie는 존재하는 상황");
-        // uuid 값
+        // remember-me의 uuid 값 추출
         String uuid = cookie.getValue();
 
         try {
             // 데이터베이스 확인
+            // 브라우저의 remember-me 쿠키 값과 MariaDB의 uuid 값이 일치하는지
             MemberDTO memberDTO = MemberService.INSTANCE.getByUUID(uuid);
 
             log.info("쿠키의 값으로 조히한 사용자 정보: " + memberDTO);
+            
+            // remember-me의 정보가 MariaDB의 uuid와 일치하지 않을 때
             if(memberDTO == null) {
                 throw new Exception("Cookie value is not valid");
             }
-            // 회원 정보를 세션에 추가
+            // 쿠키값과 DB 값이 일치하면, 회원 정보(loginInfo)를 세션에 추가
+            // 서블릿 전송
             session.setAttribute("loginInfo", memberDTO);
             filterChain.doFilter(req, resp);
 
@@ -73,10 +78,12 @@ public class LoginCheckFilter implements Filter {
             return  null;
         }
 
+        // Cookie 객체를 얻는데, 추가로 Optional에서 제공하는 메서드를 사용할 수 있음
         Optional<Cookie> result = Arrays.stream(cookies)
                 .filter(ck -> ck.getName().equals(name))
                 .findFirst();
 
+        // result가 정상이면 쿠키 객체를 리턴하고, 아니면 null 값을 리턴함
         return result.isPresent() ? result.get() : null;
     }
 }
