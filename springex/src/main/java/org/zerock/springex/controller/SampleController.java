1c259a83518882e00245dc7293c570c622c4cb2e
서혜리 <hye_dam_2@naver.com>
2024-04-15 14:35:58 +0900

    p.286까지. 예외처리

--- springex/src/main/java/org/zerock/springex/controller/SampleController.java
+++ springex/src/main/java/org/zerock/springex/controller/SampleController.java
@@ -2,8 +2,12 @@ package org.zerock.springex.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.springex.dto.TodoDTO;
 
 import java.time.LocalDate;
 
@@ -60,8 +64,64 @@ public class SampleController {
     스프링은 이것을 보다 편하게 하도록 도와줌
      */
     @GetMapping("/ex3")
-    public void e3(LocalDate dueDate) {
+    public void ex3(LocalDate dueDate) {
         log.info("ex3.............");
         log.info("dueDate: " + dueDate);
+    }
+
+    @GetMapping("/ex4")
+    public void ex4(Model model) {
+        log.info("------------------------");
+        
+        // model에 추가한 값을 꺼내서 request에 저장함
+        model.addAttribute("message", "Hello World");
+    }
+
+    /*
+    웹에서 수신된 parameter들은 todoDTO의 동일한 필드에 저장되고,
+    todoDTO라는 이름으로 request에 전달됨
+    name이라는 이름으로 Albert가 request에 전달됨
+    
+    request에 전달된다는 의미는 결국 jsp에서 꺼내어 쓸 수 있다는 의미
+     */
+    @GetMapping("ex4_1")
+    public void ex4Extra(TodoDTO todoDTO, Model model) {
+        log.info(todoDTO);
+        model.addAttribute("name", "Albert");
+    }
+
+    /*
+    todoDTO를 dto이라는 이름으로 변경해서 request에 저장함
+     */
+    @GetMapping("ex4_2")
+    public void ex4Extra2(@ModelAttribute("dto") TodoDTO todoDTO, Model model) {
+        log.info(todoDTO);
+        model.addAttribute("name", "Albert");
+    }
+
+    @GetMapping("/ex5")
+    public String ex5(RedirectAttributes redirectAttributes) {
+
+        // 리다이렉션 시 필요한 정보를 전달하는 것
+        // 주소 옆에 매개변수로 주소와 함께 parameter로 전달됨
+        redirectAttributes.addAttribute("name", "ABC");
+        
+        // 서버가 재접속명령을 브라우저한테 보냈을 때 한 번만 사용할 수 있는 일회성 데이터
+        redirectAttributes.addFlashAttribute("result", "success");
+
+        // 리턴값이 존재하면 jsp로 전달되는 것이 아니라,
+        // 브라우저한테 아래 주소로 재접속을 하도록 하는 것
+        return "redirect:/ex6";
+    }
+
+    @GetMapping("/ex6")
+    public void ex6() {
+        // WEB-INF/views/ex6.jsp로 이동함
+    }
+
+    @GetMapping("/ex7")
+    public void ex7(String p1, int p2) {
+        log.info("p1....." + p1);
+        log.info("p2....." + p2);
     }
 }
