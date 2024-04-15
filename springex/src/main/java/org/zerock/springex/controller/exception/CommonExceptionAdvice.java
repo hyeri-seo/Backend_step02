b3716ac4555c3e02cd76490ed783303240147b72
서혜리 <hye_dam_2@naver.com>
2024-04-15 14:56:49 +0900

    p.290까지. 예외처리

--- springex/src/main/java/org/zerock/springex/controller/exception/CommonExceptionAdvice.java
+++ springex/src/main/java/org/zerock/springex/controller/exception/CommonExceptionAdvice.java
@@ -1,14 +1,21 @@
 package org.zerock.springex.controller.exception;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.util.Arrays;
 
 @ControllerAdvice
 @Log4j2
 public class CommonExceptionAdvice {
 
+    // JSON 문자열 전송방식(주로 Ajax, REST api 방식에서 사용)
     // 문자열이나 JSON 데이터를 그대로 전송할 때 사용되는 어노테이션
     @ResponseBody
     @ExceptionHandler(NumberFormatException.class)
@@ -18,5 +25,39 @@ public class CommonExceptionAdvice {
         log.error(numberFormatException.getMessage());
 
         return "NUMBER FORMAT EXCEPTION";
+    }
+
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    public String exceptCommon(Exception exception){
+
+        log.error("-----------------------------------");
+        log.error(exception.getMessage());
+
+        StringBuffer buffer = new StringBuffer("<ul>");
+
+        buffer.append("<li>" +exception.getMessage()+"</li>");
+
+        Arrays.stream(exception.getStackTrace()).forEach(stackTraceElement -> {
+            buffer.append("<li>"+stackTraceElement+"</li>");
+        });
+        buffer.append("</ul>");
+
+        return buffer.toString();
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String notFound(Model model) {
+
+        /*
+        아래는 redirect: 를 주지 않음
+        그러므로 /WEB-INF/views/custom404.jsp로 이동함
+        결국 브라우저는 custom404.jsp 화면을 보게 됨
+         */
+
+        model.addAttribute("errmsg", "error");
+
+        return "custom404";
     }
 }
