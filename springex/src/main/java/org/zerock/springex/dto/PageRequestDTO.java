cb0be090416ef473ac1832f789568ce7b3adf401
서혜리 <hye_dam_2@naver.com>
2024-04-17 10:25:58 +0900

    p. 354까지. 페이징 처리

---
+++ springex/src/main/java/org/zerock/springex/dto/PageRequestDTO.java
@@ -0,0 +1,33 @@
+package org.zerock.springex.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Positive;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageRequestDTO {
+
+    @Builder.Default
+    @Min(value = 1)
+    @Positive
+    private int page = 1;       // 페이지 번호
+
+    @Builder.Default
+    @Min(value = 10)
+    @Max(value = 100)
+    @Positive
+    private int size =  10;     // 페이지당 보여줄 정보의 크기
+
+    // 해당 페이지 이전에 몇 개를 건너 뛰어야 하는지 계산하는 메서드
+    public int getSkip() {
+        return (page-1) * 10;
+    }
+}
