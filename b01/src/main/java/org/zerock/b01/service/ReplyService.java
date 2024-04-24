0e0f65529d8cb3805671184ceeeca6fe8294c581
서혜리 <hye_dam_2@naver.com>
2024-04-24 09:59:01 +0900

    p.555까지. 특정 게시물의 댓글 목록 처리

--- b01/src/main/java/org/zerock/b01/service/ReplyService.java
+++ b01/src/main/java/org/zerock/b01/service/ReplyService.java
@@ -1,5 +1,7 @@
 package org.zerock.b01.service;
 
+import org.zerock.b01.dto.PageRequestDTO;
+import org.zerock.b01.dto.PageResponseDTO;
 import org.zerock.b01.dto.ReplyDTO;
 
 public interface ReplyService {
@@ -11,4 +13,6 @@ public interface ReplyService {
     void modify(ReplyDTO replyDTO);
 
     void remove(Long rno);
+
+    PageResponseDTO<ReplyDTO> getListOfBoard(Long bno, PageRequestDTO pageRequestDTO);
 }
