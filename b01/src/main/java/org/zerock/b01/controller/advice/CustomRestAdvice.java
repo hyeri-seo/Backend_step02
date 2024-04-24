4ce315932aa34d559fbabb506c558e63de8cbef9
서혜리 <hye_dam_2@naver.com>
2024-04-24 10:22:02 +0900

    p.563까지. 특정 댓글 조회

--- b01/src/main/java/org/zerock/b01/controller/advice/CustomRestAdvice.java
+++ b01/src/main/java/org/zerock/b01/controller/advice/CustomRestAdvice.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @RestController
 @Log4j2
@@ -43,6 +44,19 @@ public class CustomRestAdvice {
 
         errorMap.put("time", ""+System.currentTimeMillis());
         errorMap.put("msg", "constraint fails");
+        return ResponseEntity.badRequest().body(errorMap);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+    public ResponseEntity<Map<String, String>> handleNoSuchElement(Exception e) {
+
+        log.error(e);
+
+        Map<String, String> errorMap = new HashMap<>();
+
+        errorMap.put("time", ""+System.currentTimeMillis());
+        errorMap.put("msg", "No Such Element Exception");
         return ResponseEntity.badRequest().body(errorMap);
     }
 }
