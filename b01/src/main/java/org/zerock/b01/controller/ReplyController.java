25c5aa4b574898c5ed8482dcced3f90e5c0e3f7e
서혜리 <hye_dam_2@naver.com>
2024-04-24 10:39:38 +0900

    p.566까지. 특정 댓글 수정

--- b01/src/main/java/org/zerock/b01/controller/ReplyController.java
+++ b01/src/main/java/org/zerock/b01/controller/ReplyController.java
@@ -79,7 +79,7 @@ public class ReplyController {
 
     @ApiOperation(value = "Modify Reply", notes = "PUT 방식으로 특정 댓글 수정")
     @PutMapping(value = "/{rno}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Map<String, Long> remove(@PathVariable("rno") Long rno,
+    public Map<String, Long> modify(@PathVariable("rno") Long rno,
                                     @RequestBody ReplyDTO replyDTO) {
 
         replyDTO.setRno(rno);   // 번호를 일치시킴
