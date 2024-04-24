3d4679b9b7ec5a957fc92671f3f1aa27ae008fde
서혜리 <hye_dam_2@naver.com>
2024-04-24 10:12:38 +0900

    p.559까지. 댓글 등록 기능 확인

--- b01/src/main/java/org/zerock/b01/controller/ReplyController.java
+++ b01/src/main/java/org/zerock/b01/controller/ReplyController.java
@@ -1,6 +1,7 @@
 package org.zerock.b01.controller;
 
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.validation.BindException;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.b01.dto.ReplyDTO;
+import org.zerock.b01.service.ReplyService;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -18,7 +20,10 @@ import java.util.Map;
 @RestController     // Rest방식/json으로 응답
 @RequestMapping("/replies")
 @Log4j2
+@RequiredArgsConstructor    // 의존성 주입을 위한
 public class ReplyController {
+
+    private final ReplyService replyService;
 
     @ApiOperation(value = "Replies POST", notes = "POST 방식으로 댓글 등록")        // swagger-ui에 보여짐
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -38,7 +43,8 @@ public class ReplyController {
         }
 
         Map<String, Long> resultMap = new HashMap<>();
-        resultMap.put("rno", 111L);
+        Long rno = replyService.register(replyDTO);
+        resultMap.put("rno", rno);
         // 브라우저한테는 200 ok 헤더정보와 함께 resultMap이 json으로 전달됨
         return resultMap;
     }
--- b01/src/main/java/org/zerock/b01/controller/advice/CustomRestAdvice.java
+++ b01/src/main/java/org/zerock/b01/controller/advice/CustomRestAdvice.java
@@ -1,6 +1,7 @@
 package org.zerock.b01.controller.advice;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
@@ -22,12 +23,26 @@ public class CustomRestAdvice {
 
         log.error(e);
         Map<String, String> errorMap = new HashMap<>();
+
         if(e.hasErrors()) {
             BindingResult bindingResult = e.getBindingResult();
             bindingResult.getFieldErrors().forEach(fieldError -> {
                 errorMap.put(fieldError.getField(), fieldError.getCode());
             });
         }
+        return ResponseEntity.badRequest().body(errorMap);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+    public ResponseEntity<Map<String, String>> handleFKException(Exception e) {
+
+        log.error(e);
+
+        Map<String, String> errorMap = new HashMap<>();
+
+        errorMap.put("time", ""+System.currentTimeMillis());
+        errorMap.put("msg", "constraint fails");
         return ResponseEntity.badRequest().body(errorMap);
     }
 }
